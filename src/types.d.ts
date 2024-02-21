7dcaff5ad591d63a5996c63dad265ac013e5d955
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-21 12:20:24 -0600

    initial commit

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+export declare type ContentFn = (
+  attrs: Attributes | Children,
+  children?: Children
+) => string;
+
+export declare type SelfClosingFn = (attrs?: Attributes) => string;
+
+export declare type StringToAny = { [key: string]: any };
+
+export declare type StringToString = { [key: string]: string };

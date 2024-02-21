fc7da0e00c4a39f558e97c91c6d340bf2a5b46c4
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2024-02-21 13:35:55 -0600

    cleanup

--- src/types.d.ts
+++ src/types.d.ts
@@ -8,7 +8,3 @@ export declare type ContentFn = (
 ) => string;
 
 export declare type SelfClosingFn = (attrs?: Attributes) => string;
-
-export declare type StringToAny = {[key: string]: any};
-
-export declare type StringToString = {[key: string]: string};
